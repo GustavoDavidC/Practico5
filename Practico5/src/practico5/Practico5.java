9711a11b009b22c0188efb920af368bf75ec83ba
PC <PC@NBK-001>
2021-04-20 23:05:35 -0300

    Primer commit

---
+++ Practico5/src/practico5/Practico5.java
@@ -0,0 +1,17 @@
+
+package practico5;
+
+/**
+ *
+ * @author PC
+ */
+public class Practico5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
