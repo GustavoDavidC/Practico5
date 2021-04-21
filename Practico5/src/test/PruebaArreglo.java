eef6f4b041f3ee2be2302ceb70602dd6efa1a314
PC <PC@NBK-001>
2021-04-20 23:12:26 -0300

    Practicamente se realizo todo el practico en este commit

---
+++ Practico5/src/test/PruebaArreglo.java
@@ -0,0 +1,23 @@
+package test;
+import Entidades.Arreglo;
+
+public class PruebaArreglo {
+
+    public static void main(String[] args) {
+       
+        int arr1[]={1,2,3,4,5,6,7,8,9};
+        
+        int arr2[][]={{1,2,3},{4,5,6},{71,8,9},{1},{15,11}};
+        
+        String vocales="y? arraca o no arranca?";
+        
+         Arreglo.sumarLista(arr1);
+       
+         Arreglo.buscarMayor(arr2);
+        
+         Arreglo.cuentaVocales(vocales);
+         
+         Arreglo.cuentaCaracter(vocales, 'r');
+    }
+    
+}
